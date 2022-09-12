40bd3f50a58a46218d69ca45fcd67fbb3cc899f5
Kritarth Kambli <kritarthkambli@gmail.com>
2022-09-12 20:39:53 +0530

    Heaps intro revision

--- Java/Programiz/Heaps/Heaps.java
+++ Java/Programiz/Heaps/Heaps.java
@@ -9,20 +9,17 @@ class Heaps {
     int largest = i;
     int l = 2 * i + 1;
     int r = 2 * i + 2;
-    if(l<size&&hT.get(l)> hT.get(largest))
+    if(l<size&&hT.get(largest)<hT.get(l))
       largest = l;
-    if(r<size&&hT.get(r)> hT.get(largest))
+    if(r<size&&hT.get(largest)<hT.get(r))
       largest = r;
     if (largest != i) {
       int temp = hT.get(largest);
-      hT.set(largest, hT.get(i));
-      hT.set(i, temp);
-
+      hT.set(largest,hT.get(i));
+      hT.set(i,temp);
       heapify(hT, largest);
     }
   }
-
-  // Function to insert an element into the tree
   void insert(ArrayList<Integer> hT, int newNum) {
     int size = hT.size();
     if (size == 0) {
@@ -30,12 +27,11 @@ class Heaps {
     }
     else {
       hT.add(newNum);
-      for (int i = size/2 - 1;i>=0 ; i--) {
-        heapify(hT, i);
+      for (int i = size/2 - 1; i >=0; i--) {
+        heapify(hT,i);
       }
     }
   }
-
   // Function to delete an element from the tree
   void deleteNode(ArrayList<Integer> hT, int num) {
     int size = hT.size();
@@ -71,10 +67,15 @@ class Heaps {
 
     Heaps h = new Heaps();
     h.insert(array, 3);
+    h.printArray(array, size);
     h.insert(array, 4);
+    h.printArray(array, size);
     h.insert(array, 9);
+    h.printArray(array, size);
     h.insert(array, 5);
+    h.printArray(array, size);
     h.insert(array, 2);
+    h.printArray(array, size);
 
     System.out.println("Max-Heap array: ");
     h.printArray(array, size);
