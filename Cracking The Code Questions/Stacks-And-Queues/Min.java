ead182f784d6760c951e6a471eb76e177dd0fd25
Kritarth Kambli <kritarthkambli@gmail.com>
2023-01-26 01:14:39 +0530

    26/01/2023

--- Cracking The Code Questions/Stacks-And-Queues/Min.java
+++ Cracking The Code Questions/Stacks-And-Queues/Min.java
@@ -2,28 +2,40 @@ import java.util.*;
 //import java.lang.*;
 
 public class Min<T> extends Stack<NodeWithMin> {
-  public void push(int min) {
-
+  public void push(int val) {
+    int min = Math.min(val, min());
+    super.push(new NodeWithMin(val, min));
   }
 
+  public int min() {
+    if (this.isEmpty()) {
+      return Integer.MAX_VALUE;
+    }
+    else {
+      return peek().min;
+    }
+  }
   
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     Min<Integer> ms = new Min<Integer>();
     int n = 4;
-    while (n >= 0) {
+    while (n > 0) {
       int ele = sc.nextInt();
       ms.push(ele);
       n--;
     }
+    System.out.println(ms.min());
     sc.close();
   }
 }
+
 class NodeWithMin {
-    int val;
-    int min;
-    NodeWithMin(int val,int min){
-      this.val = val;
-      this.min = min;
-    }
+  int val;
+  int min;
+
+  NodeWithMin(int val, int min) {
+    this.val = val;
+    this.min = min;
+  }
 }
